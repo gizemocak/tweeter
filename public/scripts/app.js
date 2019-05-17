f54d29ac55bd80c336835abd1554eaafacdb3df2
gizemocak <gizem_ocak@outlook.com>
2019-05-17 20:09:27 +0000

    installed scss and created scss files

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,7 +1,35 @@
 /*
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+//Helper function for the time output 
+function timeDiff(time1, time2) {
+
+  const msPerMinute = 60 * 1000;
+  const msPerHour = msPerMinute * 60;
+  const msPerDay = msPerHour * 24;
+  const msPerMonth = msPerDay * 30;
+  const msPerYear = msPerDay * 365;
+
+  let diff = time1 - time2;
+
+  if (diff < msPerMinute) {
+    return Math.round(diff / 1000) + ' seconds ago';
+  } else if (diff < msPerHour) {
+    return Math.round(diff / msPerMinute) + ' minutes ago';
+  } else if (diff < msPerDay) {
+    return Math.round(diff / msPerHour) + ' hours ago';
+  } else if (diff < msPerMonth) {
+    return Math.round(diff / msPerDay) + ' days ago';
+  } else if (diff < msPerYear) {
+    return Math.round(diff / msPerMonth) + ' months ago';
+  } else {
+    return Math.round(diff / msPerYear) + ' years ago';
+  }
+}
+
 function createTweetElement(tweet) {
+  const tweetDate = timeDiff(Date.now(), tweet.created_at);
+
   let $header = $("<header>")
   $header.append($("<img>").attr("src", tweet.user.avatars.small))
   $header.append($("<h1>").text(tweet.user.name))
@@ -14,7 +42,7 @@ function createTweetElement(tweet) {
   let $flag = $("<i>").attr("href", "#").addClass("fas fa-flag font-awesome")
   let $retweet = $("<i>").attr("href", "#").addClass("fas fa-retweet font-awesome")
   let $heart = $("<i>").attr("href", "#").addClass("fas fa-heart font-awesome")
-  let $date = $("<div>").text(tweet.created_at)
+  let $date = $("<div>").text(tweetDate)
   $icons.append($flag).append($retweet).append($heart)
   $footer.append($date).append($icons)
 
@@ -37,7 +65,7 @@ function renderTweets(tweets) {
 function ajaxRequest() {
   $("form").on("submit", function (event) {
     event.preventDefault();
-    const textarea = $("textarea").val()
+    const textarea = $("textarea").val().trim()
     if (textarea === null || textarea === "") {
       $(".error-message").text("Plase Enter a Tweet!").slideDown(400)
 
@@ -56,6 +84,8 @@ function ajaxRequest() {
           console.log("Post success")
           loadTweets()
         })
+      $("textarea").val("")
+      $("span.counter").text("140")
     }
   })
 }
@@ -71,8 +101,9 @@ function loadTweets() {
 }
 
 $(document).ready(function () {
+  $(".new-tweet").hide()
   $(".error-message").hide()
-  $("#nav-bar button").on("click", function () {
+  $("#nav-bar button.compose").on("click", function () {
     $(".new-tweet").slideToggle(400, function () {
       $("textarea").focus()
     })
